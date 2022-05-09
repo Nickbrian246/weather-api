a035a9cc0908574ba5ad54ab8401f6441e31b116
Nick Brian <nbjm1234@outlook.com>
2022-05-09 15:42:01 -0500

    se agregan librerias, react router, react redux, antd design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function App() {
   return (
     <div className="App">
-    
+     <p> hello there</p>
     </div>
   );
 }
